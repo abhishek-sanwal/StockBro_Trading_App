ccd585968ef200b00b95e3746a604fc16a29d18e
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-08-17 11:46:06 +0530

    Added redis as channels layer

--- StockBro/StockBro/settings.py
+++ StockBro/StockBro/settings.py
@@ -149,6 +149,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
+            # Configuring redis as channel layer
             "hosts": [('127.0.0.1', 6379)],
         },
     },
